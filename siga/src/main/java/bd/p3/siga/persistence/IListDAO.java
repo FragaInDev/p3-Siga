498e7db6337ce6671646fa6d31d21ba8eac360f8
Bruno Fraga <100775696+FragaInDev@users.noreply.github.com>
2023-06-05 12:46:25 -0300

    Delete siga directory

--- siga/src/main/java/bd/p3/siga/persistence/IListDAO.java
+++
@@ -1,8 +0,0 @@
-package bd.p3.siga.persistence;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface IListDAO<T> {
-	public List<T> list() throws SQLException, ClassNotFoundException;
-}
